a46e41a7a797f6b117f18a31ee4e5bbd21ba7a07
gawahlb <wah23001@byui.edu>
2024-12-09 14:22:51 -0700

    finished formatting for home page

---
+++ src/js/utils.mjs
@@ -0,0 +1,26 @@
+export function renderWithTemplate(
+    template,
+    parent,
+    data,
+    callback
+) {
+    parent.insertAdjacentHTML('afterbegin', template.innerHTML);
+    if(callback) callback(data);
+}
+
+export async function loadTemplate(path) {
+    const html = await fetch(path).then(res => res.text());
+    const template = document.createElement('template');
+    template.innerHTML = html;
+    return template;
+  }
+
+export async function loadHeaderFooter() {
+    const headerPartial = await loadTemplate('/partials/header.html');
+    const footerPartial = await loadTemplate('/partials/footer.html');
+    const header = document.getElementById('header');
+    const footer = document.getElementById('footer');
+
+    renderWithTemplate(headerPartial, header);
+    renderWithTemplate(footerPartial, footer);
+}
