57683e9db5307889e0d80e9428c9ee74fbf590c9
gawahlb <wah23001@byui.edu>
2024-12-12 12:41:36 -0700

    added compare page to vite.config

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
       rollupOptions: {
         input: {
           main: resolve(__dirname, 'src/index.html'),
-          
+          compare: resolve(__dirname, 'src/compare/index.html')
         },
       },
     },
